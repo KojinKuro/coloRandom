7f2a7eea4c29d7b8f8d02b6caaeecf65fbb3ba10
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-02-21 13:34:00 -0700

    doc: Adds pesudo code for hexcode generation, stagin colors for button event, and button event to update the colors on the page.

--- main.js
+++ main.js
@@ -25,3 +25,11 @@ function isHexCode(string) {
   const hexCodeRegex = new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
   return hexCodeRegex.test(string);
 }
+
+/* - add a button that generates 5 hex codes
+grab 6 characters from a string that contains valid hexcode integers
+  done in a for loop that loops 6 times each time adding a new character to the string
+  return hexcode*/
+
+/* function that  calls hexcode generator and set hexbox 5 times*/
+/* event that calls that function when a button is clicked */
